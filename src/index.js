3cea6acf55fefa2999f276921ebb6fd73d6773b5
Vera Butler <verabutlerdesign@gmail.com>
2019-02-27 14:43:49 -0500

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,49 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import ReacDOM from 'react-dom';
+import faker from 'faker';
+import CommentDetail from './components/CommentDetail';
+import ApprovalCard from './components/ApprovalCard';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const App = () => {
+    return (
+        <div className="ui container comments">
+            <ApprovalCard>
+                <CommentDetail
+                author={faker.name.firstName()}
+                timeAgo="Today at 5PM"
+                avatar={faker.image.avatar()}
+                content={faker.lorem.sentence()} />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail
+                author={faker.name.firstName()}
+                timeAgo="Today at 7AM"
+                avatar={faker.image.avatar()}
+                content={faker.lorem.sentence()} />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail
+                author={faker.name.firstName()}
+                timeAgo="3d Ago"
+                avatar={faker.image.avatar()}
+                    content={faker.lorem.sentence()} />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail
+                author={faker.name.firstName()}
+                timeAgo="5d Ago"
+                avatar={faker.image.avatar()}
+                content={faker.lorem.sentence()} />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail
+                author={faker.name.firstName()}
+                timeAgo="1m Ago"
+                avatar={faker.image.avatar()}
+                content={faker.lorem.sentence()} />
+            </ApprovalCard>
+         </div>
+        );
+};
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReacDOM.render(<App />, document.querySelector('#root'));
